3147151e920be39141b3a7195ef4efbe18329196
Clam119 <Christopher.lam119@gmail.com>
2022-04-07 18:49:25 +0100

    First iteration complete - css added & functionality to add new to-do-list items added

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require('ejs');
+const date = require(__dirname + "/date.js");
 const app = express();
-let items = ["Hashnode blog", "Portfolio Items", "Portfolio Website"];
+const items = ["ハスノードへのブログを作成すること", "自作のプロジェクトを作成すること", "ポートフォリオサイトを作成すること"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get('/', function (req, res) {
 
-    let today = new Date();
-    let options = {
-        weekday: 'long',
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("ja-JP", options);
+    let day = date.getDate();
 
     res.render('list', {
-        typeOfDay: day,
+        listTitle: day,
         newListItems: items
     });
 
@@ -29,10 +25,29 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
 
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
 
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+});
+
+app.get("/work", function (req, res) {
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
     });
+})
+
+app.post("/work", function (req, res) {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
 
 app.listen(3000, function () {
     console.log("Successfully connected to port 3000.");
